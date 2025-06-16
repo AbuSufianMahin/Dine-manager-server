a9ad562a8ef5f0f96e49fd41cec54ad51052155c
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-06-16 23:53:31 +0600

    added api to add food without token

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,12 +8,49 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Restaurant Management website is cooking!")
 })
 
 
-app.listen(port, ()=>{
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.RESTURAUNT_USER}:${process.env.RESTURAUNT_PASS}@cluster0.udgfocl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const database = client.db("Restaurant_managementDB");
+        const foodCollection = database.collection("foodCollection");
+        const orderCollection = database.collection("orderCollection");
+
+        app.post('/add-food', async (req, res) => {
+            const foodDetails = req.body;
+            const result = await foodCollection.insertOne(foodDetails);
+            res.send(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, () => {
     console.log("Restaurant Management website is running on port:", port);
 })
 
